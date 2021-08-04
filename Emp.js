5beb11088473cc033c901d1fe612dbd756906d15
shubhasankar <ssbhaumikdmr@gmail.com>
2021-08-04 22:06:18 +0530

    Added: Store the Daily Wage along with the Total Wage

--- Emp.js
+++ Emp.js
@@ -1,4 +1,5 @@
-// Added number of working hours equal to 160 and working days 20
+//Store the Daily Wage along with the Total Wage
+
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
 const PART_TIME_HRS = 4;
@@ -6,6 +7,7 @@ const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 const MAX_WORK_HRS = 160;
 const Number_Of_Working_Days = 20;
+
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -18,14 +20,20 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
+
+function CalculateWage(empHrs) {
+    return empHrs * WAGE_PER_HR;
+}
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
 while (totalEmpHrs < MAX_WORK_HRS && totalWorkingDays < Number_Of_Working_Days) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
+    empDailyWageArr.push(CalculateWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HR;
+let totalEmpWage = CalculateWage(totalEmpHrs);
 console.log("Total days: " + totalWorkingDays +
-    " Total hrs: " + totalEmpHrs + " emp wage: " + empWage);
+    " Total hrs: " + totalEmpHrs + " emp wage: " + totalEmpWage);
