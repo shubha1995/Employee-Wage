d904618efbcb599b857bd54a3ce5d66f2bc75492
shubhasankar <ssbhaumikdmr@gmail.com>
2021-08-04 21:53:05 +0530

    Added: Assuming 20 Working Days in a Month

--- Emp.js
+++ Emp.js
@@ -1,28 +1,26 @@
-//Refactor The Code To Write Function
-
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-
+//Assuming 20 Working Days in a Month
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+const Number_Of_Working_Days = 20;
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
-            return PART_TIME_HOURS;
+            return PART_TIME_HRS;
             break;
         case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
+            return FULL_TIME_HRS;
             break;
         default:
             return 0;
     }
 }
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;   
-let empWage = getWorkingHours(empCheck) * WAGE_PER_HOUR;
+let totalEmpHrs = 0;
+for (let day = 0; day < Number_Of_Working_Days; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HR;
 console.log("Emp wage:" + empWage);
-
-
-
-
